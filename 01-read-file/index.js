3363a3772d4bffda622de0eded2366f5c8ee1dc3
Marie Serykh <marie.serykh@gmail.com>
2021-11-05 13:07:42 +0100

    fix: fix code style and variable naming

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,16 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 const fileName = 'text.txt';
-const dirPath = path.join(__dirname, fileName);
+const filePath = path.join(__dirname, fileName);
 
-const stream = new fs.ReadStream(dirPath, {encoding: 'utf-8'});
+const stream = new fs.ReadStream(filePath, {encoding: 'utf-8'});
 
 stream.on('readable', function() {
   const data = stream.read();
   if (data) {
     console.log(data);
   }
-
 });
 
 stream.on('end', function() {
